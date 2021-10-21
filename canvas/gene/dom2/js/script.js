e7a0530f485e0aa588681f21054ff401e6a682bd
hellojaehyeok <wakij6587@naver.com>
2021-10-21 16:21:28 +0900

    테이블 생성, 그래프 변경에 따른 테이블 변경 이벤트 구현, 그래프 캡쳐기능 추가

--- canvas/gene/dom2/js/script.js
+++ canvas/gene/dom2/js/script.js
@@ -1,18 +1,29 @@
 
 
+// const percent = 100/272602287;
+// console.log(percent * (
+//     (26107688 - 18185663)+
+//     (26297316 - 26107688)+
+//     (47503017 - 31185327)+
+//     (170085342 - 161052341)+
+//     (217668184 - 170085342)+
+//     (238692928 - 217668184))
+// )
 
 
-
-
-
+// ------------------------------------------------------
+//                         그래프
+// ------------------------------------------------------
 
 var xlsxData = [];
 var graphWrap = document.querySelector(".graphWrap");
 var chartDataArr = {};
 var chrTotalData = {};
 var chrTotalIndex = 0; 
+var chrPercent = {};
 var clickX = 0;
 var clickY = 0;
+
 
 // # . 1 엑셀 data 가져오기
 function readExcel() {
@@ -36,7 +47,7 @@ function parsingData(xlsxData){
         for(var key in xlsxData[i]){
             if(key == "f" || key == "m" || key == "__EMPTY" || key == "pos" || key == "chr"){continue;}
             chartDataArr[key] = chartDataArr[key] ? chartDataArr[key] : [];
-            
+            chrPercent[key] = {};
             chartDataArr[key].push({ tpye : xlsxData[i][key], pos : xlsxData[i]["pos"], chr : xlsxData[i]["chr"]});
         }
 
@@ -48,21 +59,50 @@ function parsingData(xlsxData){
         chrTotalIndex += chrTotalData[key];
     }
 
-    buildGraph(chrTotalData, chrTotalIndex);
+    buildGraph();
 } 
 
-// # . 그래프 생성
-function buildGraph(chrTotalData, chrTotalIndex){
+// # . 3 그래프 생성
+function buildGraph(){
+
+    function makeParent(className){
+        var graphElName = document.createElement('div');
+        graphElName.classList.add("graphElName");
+        var divEl = document.createElement('div');
+        divEl.classList.add(className);
+        
+        var graphName = document.createElement('div');
+        graphName.innerText = className;
+        graphName.classList.add("graphName");
+
+        graphElName.appendChild( graphName ); 
+        graphElName.appendChild( divEl ); 
+        graphWrap.appendChild( graphElName ); 
+    }
+    makeParent("mother");
+    makeParent("father");
 
     // 클 틀 생성
     for(var key in chartDataArr){
+        var graphElName = document.createElement('div');
+        graphElName.classList.add("graphElName");
+
         var divEl = document.createElement('div');
         divEl.setAttribute("data-column", key);
         divEl.classList.add("graphEl");
-        graphWrap.appendChild( divEl );   
+
+        var graphName = document.createElement('div');
+        graphName.innerText = key;
+        graphName.classList.add("graphName");
+
+        graphElName.appendChild( graphName ); 
+        graphElName.appendChild( divEl ); 
+        graphWrap.appendChild( graphElName ); 
         
+
         // 작은 틀 생성
         for(var keySmall in chrTotalData){
+            // if(typeof(chrTotalData[keySmall])!=="number"){continue;}
             var divElSmall = document.createElement('div');
             divElSmall.setAttribute("data-column", keySmall);
             divElSmall.classList.add("chrStack");
@@ -72,12 +112,12 @@ function buildGraph(chrTotalData, chrTotalIndex){
         
         // 작은거 안에 작은거 생성 
         for(var i = 0 ; i < chartDataArr[key].length ; i++){
-
             var divElSmallStack = document.createElement('div');
             divElSmallStack.setAttribute("data-pos", chartDataArr[key][i]["pos"]);
             divElSmallStack.setAttribute("data-chr", chartDataArr[key][i]["chr"]);
             divElSmallStack.setAttribute("data-tpye", chartDataArr[key][i]["tpye"]);
             divElSmallStack.classList.add("chrStackSmall");
+
 
             // 높이값
             var gap = 0;
@@ -90,25 +130,65 @@ function buildGraph(chrTotalData, chrTotalIndex){
                     gap = chartDataArr[key][i]["pos"];
                 }
             }
-            var divElSmall = divEl.querySelector("[data-column=" +chartDataArr[key][i]["chr"]+ "]");
-            divElSmallStack.style.height = (  100/chrTotalData[chartDataArr[key][i]["chr"]]  ) * gap + "%";
+
+            var currentChr = chartDataArr[key][i]["chr"];
+            var divElSmall = divEl.querySelector("[data-column=" +currentChr+ "]");
+        
+            chrPercent[key][currentChr] = chrPercent[key][currentChr] ? chrPercent[key][currentChr] : 0;
+
+            divElSmallStack.style.height = (  100/chrTotalData[currentChr]  ) * gap + "%";
             
             // 배경색
             if(chartDataArr[key][i]["tpye"] == "A"){
+                chrPercent[key][currentChr] += (100/chrTotalData[chartDataArr[key][i]["chr"]]) * gap;
                 divElSmallStack.style.backgroundColor= "#FF9BBD"
             }else if(chartDataArr[key][i]["tpye"] == "B"){
                 divElSmallStack.style.backgroundColor= "#95CAFF"
             }else{
                 divElSmallStack.style.backgroundColor= "#FFE395"
             }
-            
             divElSmall.appendChild( divElSmallStack );
         }
-    }
+    }    
+
+
+    // var graphContainer = document.querySelector(".graphContainer");
+
+    // // 작은 틀 생성
+    // for(var keySmall in chrTotalData){
+    //     var divElSmall = document.createElement('div');
+    //     divElSmall.classList.add("chrStack");
+    //     divElSmall.style.height = (100/chrTotalIndex) * chrTotalData[keySmall] + "%";
+    //     graphContainer.appendChild( divElSmall );
+    // }
+
+    
+
+    // // 작은 틀 생성
+    // var graphContainer = document.querySelector(".graphContainer");
+
+    // var topPercent = 0;
+    // for(var key in chrTotalData){
+    //     if(typeof(chrTotalData[key])!=="number"){continue}
+
+    //     topPercent += (100/chrTotalIndex) * chrTotalData[key]
+
+    //     var dotEl = document.createElement('div');
+    //     dotEl.classList.add("dotline");
+    //     dotEl.style.top =  (topPercent * 4.9) + 0 +"px";
+    //     graphContainer.appendChild( dotEl );
+
+    //     console.log(key);
+    //     console.log((topPercent * 4.9) + 0)
+    //     // console.log(topPercent);
+    // }
+
+
+    buildTable();
 }
 
 
-
+// 드래그&드롭 인터랙션
 var isMouseMove = false;
 var clickColumn = "";
 graphWrap.addEventListener("mousedown", function(e){
@@ -124,7 +204,7 @@ graphWrap.addEventListener("mousedown", function(e){
 
 graphWrap.addEventListener("mousemove", function(e){
     if(!isMouseMove){return};
-    var clickEl = document.querySelector("[data-column=" +clickColumn+ "]");
+    var clickEl = document.querySelector("[data-column=" +clickColumn+ "]").parentNode;
     clickEl.style.position = "absolute";
     clickEl.style.top = `${e.clientY - clickY}px`;
     clickEl.style.left = `${e.clientX - clickX}px`;
@@ -133,8 +213,7 @@ graphWrap.addEventListener("mousemove", function(e){
 
     // 그림자
     if(!e.path[2].dataset.column){return;}
-
-    var currentEl = document.querySelector("[data-column=" +e.path[2].dataset.column+ "]");
+    var currentEl = document.querySelector("[data-column=" +e.path[2].dataset.column+ "]").parentNode;
     var currentShadow = document.querySelector(".graphShadow");
     if(currentShadow){
         currentShadow.remove();
@@ -142,6 +221,7 @@ graphWrap.addEventListener("mousemove", function(e){
 
     var shadowGraph = document.createElement('div');
     shadowGraph.classList.add("graphShadow");
+
     clickEl.parentNode.insertBefore(shadowGraph, currentEl)
 
 });
@@ -149,7 +229,7 @@ graphWrap.addEventListener("mousemove", function(e){
 graphWrap.addEventListener("mouseup", function(e){
     isMouseMove = false;
     if(!clickColumn){return;}
-    var clickEl = document.querySelector("[data-column=" +clickColumn+ "]");
+    var clickEl = document.querySelector("[data-column=" +clickColumn+ "]").parentNode;
     var currentShadow = document.querySelector(".graphShadow");
     if(!currentShadow){return}
 
@@ -159,5 +239,117 @@ graphWrap.addEventListener("mouseup", function(e){
     clickEl.style.pointerEvents = "all";
 
     clickEl.parentNode.replaceChild(clickEl, currentShadow);
+    sortGraphData();
 });
 
+
+// 그래프 정렬
+function sortGraphData(){
+    var newChrArr = [];
+    var graphEls = document.querySelectorAll(".graphEl");
+
+    for(var i = 0 ; i < graphEls.length ; i++){
+        newChrArr.push(graphEls[i].dataset.column)
+    }
+    var newChrObj = {};
+
+    for(var i = 0 ; i < newChrArr.length ; i++){
+        newChrObj[newChrArr[i]] = chartDataArr[newChrArr[i]];
+    }
+    chartDataArr = newChrObj;
+    buildTable();
+}
+
+
+
+
+
+
+// ------------------------------------------------------
+//                         테이블
+// ------------------------------------------------------
+var isTable = false;
+function buildTable(){
+    var table = document.querySelector('.table');
+    isTable = true;
+    // 초기화
+    var tableContent = document.querySelector('.tableContent');
+    table.remove();
+
+    var newTable = document.createElement('table');
+    newTable.classList.add("table");
+    
+
+    // 머리
+    var thead = document.createElement('thead');
+    var theadTr = document.createElement('tr');
+    var theadTh = document.createElement('th');
+    theadTr.appendChild(theadTh);
+    for(var key in chrTotalData){
+        var theadTh = document.createElement('th');
+        theadTh.innerText = key;
+        theadTr.appendChild(theadTh);
+    }
+    thead.appendChild(theadTr);
+    
+    
+
+
+    // 몸통
+    var tbody = document.createElement('tbody');
+    for(var key in chartDataArr){
+        var tbodyTr = document.createElement('tr');
+        var tbodyTdName = document.createElement('td');
+        tbodyTdName.innerText = key;
+        tbodyTr.setAttribute("data-column", key + "_table");
+        tbodyTr.appendChild(tbodyTdName);
+        tbody.appendChild(tbodyTr);
+
+        for(var keySmall in chrPercent[key]){
+            var tbodyTd = document.createElement('td');
+            tbodyTd.innerText = chrPercent[key][keySmall].toFixed(2) + "%";
+            tbodyTd.style.backgroundColor = "hsl("+ (250-(chrPercent[key][keySmall].toFixed(2) * 2.5)) +", 100%, 57%)";
+            tbodyTr.appendChild(tbodyTd);
+        }
+    }
+
+    
+    newTable.appendChild(thead);
+    newTable.appendChild(tbody);
+
+
+    tableContent.appendChild(newTable);
+
+}
+
+
+
+
+
+
+
+// ------------------------------------------------------
+//                         사진찍기
+// ------------------------------------------------------
+function downloadGraphImg(e){
+    var graphContainer = document.querySelector(".graphContainer");
+    graphContainer.style.width = "unset";
+    graphContainer.style.height = "unset";
+
+    html2canvas(graphWrap).then(function(canvas){
+        var myImage = canvas.toDataURL();
+        downloadURI(myImage, "저장이미지이름.png") 
+    });
+}
+function downloadURI(uri, name){
+	var link = document.createElement("a")
+	link.download = name;
+	link.href = uri;
+	document.body.appendChild(link);
+	link.click();
+
+    var graphContainer = document.querySelector(".graphContainer");
+    graphContainer.style.width = "700px";
+    graphContainer.style.height = "600px";
+
+}
--- canvas/gene/heatmap/js/script.js
+++ canvas/gene/heatmap/js/script.js
@@ -4,6 +4,6 @@
 var heatmapBoxEls = document.querySelectorAll(".heatmapBox");
 
 for(var i = 0 ; i < heatmapBoxEls.length ; i++){
-    heatmapBoxEls[i].style.backgroundColor = "hsl("+ Number(heatmapBoxEls[i].innerHTML.replace("%", "")) * 2.5 +", 100%, 57%)";
+    heatmapBoxEls[i].style.backgroundColor = "hsl("+ (250-(Number(heatmapBoxEls[i].innerHTML.replace("%", "")) * 2.5)) +", 100%, 57%)";
 }
    
