facc12e3bd32727895aa1705cb9f784ded712551
hellojaehyeok <wakij6587@naver.com>
2021-10-26 10:20:34 +0900

    유전자 그래프  vanila_gene_chart 레파지토리로 따로 분리

--- canvas/gene/heatmap/js/script.js
+++
@@ -1,9 +0,0 @@
-
-
-
-var heatmapBoxEls = document.querySelectorAll(".heatmapBox");
-
-for(var i = 0 ; i < heatmapBoxEls.length ; i++){
-    heatmapBoxEls[i].style.backgroundColor = "hsl("+ (250-(Number(heatmapBoxEls[i].innerHTML.replace("%", "")) * 2.5)) +", 100%, 57%)";
-}
-   
